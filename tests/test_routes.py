9c9ffbd802ff0cf3782ad04b8af52a311e82f27b
Jef <jdovalov@gmail.com>
2025-03-22 17:48:48 -0400

    Add unit test to query products by availability

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -264,6 +264,19 @@ class TestProductRoutes(TestCase):
         for product in data:
             self.assertEqual(product["category"], category.name)
 
+    def test_query_by_availability(self):
+        """It should Query Products by category"""
+        products = self._create_products(10)
+        expected_count = len([product for product in products if product.available is True])
+
+        # request the data
+        response = self.client.get(BASE_URL, query_string=f"available=true")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), expected_count)
+        for product in data:
+            self.assertEqual(product["available"], True)
+
     ######################################################################
     # Utility functions
     ######################################################################
